bc0f55b8e1eb343d63680a8e131a24989f74a6ac
malecks <alex@alexmathers.com>
2023-01-15 11:04:04 -0800

    remove all files and replace with aarsvp app, minus firebase stuff

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Monogram from "./Monogram"
+import illustration from "../img/Ellicottville_House_2.png"
+
+const Header = () => {
+    return (
+        <header>
+            <Monogram />
+            <img src={illustration} id="house-illustration" />
+            <h3>July 8th, 2023 — Ellicottville N.Y.</h3>
+        </header>
+    )
+}
+
+export default Header
