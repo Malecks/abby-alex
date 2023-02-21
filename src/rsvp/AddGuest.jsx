4105fb7326bab9398f26e320be1b80df6f70e1e6
malecks <alex@alexmathers.com>
2023-02-20 19:44:08 -0800

    Update add guest, working greeeaaaat

--- src/rsvp/AddGuest.jsx
+++ src/rsvp/AddGuest.jsx
@@ -1,12 +1,53 @@
 import { useEffect, useState } from 'react'
-import { Formik, Field, Form } from 'formik'
+import { useFormik } from 'formik'
 import { addParty, addGuest, getParties } from './FirebaseActions'
+
+import { CssVarsProvider } from '@mui/joy/styles';
+import { rsvpTheme } from './MuiTheme';
+import { Input, Button, Divider } from '@mui/joy'
 
 import './Rsvp.css'
 
 function AddGuest() {
 
     const [parties, setParties] = useState([])
+
+    const formikAddParty = useFormik({
+        initialValues: {
+            partyName: '',
+        },
+        onSubmit: async (values, helpers) => {
+            console.log('add party: ', values.partyName)
+            await addParty(values.partyName)
+            await updateParties()
+            helpers.resetForm()
+        }
+    })
+
+    const formikAddGuest = useFormik({
+        initialValues: {
+            party: '',
+            first: '',
+            last: '',
+            email: '',
+        },
+        // validationSchema: validationSchema,
+        onSubmit: async (values, helpers) => {
+            console.log('add guest: ', values)
+            await addGuest({
+                party: values.party,
+                first: values.first,
+                last: values.last,
+                email: values.email,
+                fridayEvent: '',
+                ceremony: '',
+                sundayEvent: '',
+                entre: '',
+                notes: ''
+            })
+            helpers.resetForm()
+        },
+    });
 
     const updateParties = async () => {
         const data = await getParties()
@@ -17,72 +58,67 @@ function AddGuest() {
         updateParties()
     }, [])
 
-    return (
-        <div className='rsvpWrapper'>
+    return (<>
+        <CssVarsProvider theme={rsvpTheme}>
             <h4>Add a party</h4>
-            <Formik
-                initialValues={{
-                    partyName:'',
-                }}
-                onSubmit={async (values) => {
-                    addParty(values.partyName)
-                }}
-            >
-                {({ isSubmitting }) => (
-                    <Form>
-                        <Field id='partyName' name='partyName' placeholder='Party name' />
-                        <button type='submit' disabled={isSubmitting}>Add party</button>
-                    </Form>
-                )}
-            </Formik>
-
-            <hr />
+            <form onSubmit={formikAddParty.handleSubmit}>
+                <Input 
+                    size='lg'
+                    id='partyName'
+                    value={formikAddParty.values.partyName}
+                    onChange={formikAddParty.handleChange}
+                    error={formikAddParty.touched.partyName && Boolean(formikAddParty.errors.partyName)}
+                    placeholder='Party name'
+                />
+                <Button type='submit'>Add party</Button>
+            </form>
+            <Divider sx={{marginTop: 4, marginBottom: 4}}/>
             <h4>Add a guest</h4>
-            <Formik
-                initialValues={{
-                    party: '',
-                    first: '',
-                    last: '',
-                    email: '',
-                    // fridayEvent: '',
-                    // ceremony: '',
-                    // sundayEvent: '',
-                    // entre: '',
-                    // notes: ''
-                }}
-                onSubmit={ async (values) => {
-                   await addGuest({
-                        party: values.party,
-                        first: values.first,
-                        last: values.last,
-                        email: values.email,
-                        fridayEvent: '',
-                        ceremony: '',
-                        sundayEvent: '',
-                        entre: '',
-                        notes: ''
-                    })
-                }}
-            >
-                {({ isSubmitting }) => (
-                    <Form>
+            <form onSubmit={formikAddGuest.handleSubmit}>
+                <select 
+                    as="select" 
+                    size='lg'
+                    id='party'
+                    onChange={formikAddGuest.handleChange}
+                >
+                    { parties.map((party) => {
+                        return <option value={party.id} key={party.id}> {party.partyName} </option>
+                    })}
+                </select>
 
-                        <Field as='select' name='party'>
-                            <option disabled value=''>Party name</option>
-                            { parties.map((party) => {
-                             return <option value={party.id} key={party.id}> {party.partyName} </option>
-                            })}
-                        </Field>
-                        {/* <Field id='party' name='party' placeholder='Party name' /> */}
-                        <Field id='first' name='first' placeholder='First name' />
-                        <Field id='last' name='last' placeholder='Last name' />
-                        <Field id='email' name='email' placeholder='Email' />
-                        <button type='submit' disabled={isSubmitting}>Add guest</button>
-                    </Form>
-                )}
-            </Formik>
-        </div>
-    )
+                <Input 
+                    size='lg'
+                    id='first'
+                    value={formikAddGuest.values.first}
+                    onChange={formikAddGuest.handleChange}
+                    error={formikAddGuest.touched.first && Boolean(formikAddGuest.errors.first)}
+                    placeholder='First name'
+                    // helperText={formik.touched.first && formik.errors.first}
+                />
+
+                <Input 
+                    size='lg'
+                    id='last'
+                    value={formikAddGuest.values.last}
+                    onChange={formikAddGuest.handleChange}
+                    error={formikAddGuest.touched.last && Boolean(formikAddGuest.errors.last)}
+                    placeholder='Last name'
+                    // helperText={formik.touched.last && formik.errors.last}
+                />
+
+                <Input 
+                    size='lg'
+                    id='email'
+                    value={formikAddGuest.values.email}
+                    onChange={formikAddGuest.handleChange}
+                    error={formikAddGuest.touched.email && Boolean(formikAddGuest.errors.email)}
+                    placeholder='Email'
+                    // helperText={formik.touched.email && formik.errors.email}
+                />
+                <Button type='submit'>Add guest</Button>
+            </form>
+        </CssVarsProvider>
+    </>)
 }
 
 export default AddGuest
